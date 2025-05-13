a6d78584cc10b67b9502d7f161a416b65a14a199
Nabila Sultana Eusha <eusha.nabila@gmail.com>
2025-05-13 18:30:04 +0600

    started frontendd

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,49 @@
 
+import './App.css'
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Myaccount from "./pages/Myaccount";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import {RouterProvider, createBrowserRouter, Outlet} from "react-router-dom";
+import Announcement from "./components/Announcement";
 
 function App() {
- 
 
+  const Layout = () => {
+    return <div>
+      <Announcement />
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>;
+  };   
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Layout />,
+      children:[
+        {
+           path: "/",
+           element: <Home />
+        },
+        {
+           path: "/cart",
+           element: <Cart />
+        },
+        {
+           path: "/myaccount",
+           element: <Myaccount />
+        }
+      ]
+    },
+  ]);
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
